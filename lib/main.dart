f37e8ca5fc2c78e350617fc92d88c571ffc8bc5c
LiquidFlare <ivinjoelabraham@gmail.com>
2023-04-23 21:47:06 +0530

    Add login functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,40 @@
 import 'package:flutter/material.dart';
-import 'pages/login_page.dart';
+import 'pages/login.dart';
+import 'pages/home.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(const MyApp());
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(const Main());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+final navigatorKey = GlobalKey<NavigatorState>();
+
+class Main extends StatelessWidget {
+  const Main({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: LoginPage(),
-    );
-  }
+  Widget build(BuildContext context) => MaterialApp(
+    navigatorKey: navigatorKey,
+    home: StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return const Center(child: Text('Something went wrong try again!'));
+        } else if (snapshot.hasData) {
+          return const Home();
+        } else {
+          return const Login();
+        }
+
+      }
+    ),  
+  );
 }
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -2,21 +2,61 @@ import 'package:flutter/material.dart';
 import 'package:impact_circle/components/my_button.dart';
 import 'package:impact_circle/components/my_textfield.dart';
 import 'package:impact_circle/components/square_tile.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:impact_circle/main.dart';
 
-class LoginPage extends StatelessWidget {
-  LoginPage({super.key});
+class Login extends StatefulWidget {
+  // final VoidCallback onClickedSignUp;
 
-  // text editing controllers
-  final usernameController = TextEditingController();
+  const Login({
+    Key? key,
+    // required this.onClickedSignUp,
+  }) : super(key: key);
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  
+  final emailController = TextEditingController();
   final passwordController = TextEditingController();
 
-  // sign user in method
-  void signUserIn() {}
+  // Sign in method
+  Future signIn() async {
+    
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) => const Center(child: CircularProgressIndicator()),
+    );
+
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: emailController.text,
+        password: passwordController.text,
+      );
+    } on FirebaseException catch (e) {
+      print(e);
+    }
+
+    navigatorKey.currentState!.popUntil((route) => route.isFirst);
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(111, 19, 110, 170),
+      backgroundColor: const Color.fromARGB(111, 19, 110, 170),
       body: SafeArea(
         child: Center(
           child: SingleChildScrollView(
@@ -24,82 +64,83 @@ class LoginPage extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 const SizedBox(height: 50),
-          
-                // logo
+
+                // Logo
                 Stack(
-            alignment: Alignment.center,
-            children: [
-              Icon(
-                Icons.circle_outlined,
-                size: 200,
-                color: Colors.white,
-              ),
-              Text(
-                'IMPACT',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                 fontSize: 26,
+                  alignment: Alignment.center,
+                  children: const [
+                    Icon(
+                      Icons.circle_outlined,
+                      size: 200,
+                      color: Colors.white,
+                    ),
+                    Text(
+                      'IMPACT',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 26,
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-            ],
-          )
-          ,
-          
+
                 const SizedBox(height: 50),
-          
-                // welcome back, you've been missed!
-                Text(
+
+                // Welcome back, you've been missed!
+                const Text(
                   'Take part in making society better',
                   style: TextStyle(
                     color: Color.fromARGB(255, 255, 255, 255),
                     fontSize: 16,
                   ),
                 ),
-          
+
                 const SizedBox(height: 25),
-          
-                // username textfield
+
+                // Username textfield
                 MyTextField(
-                  controller: usernameController,
+                  controller: emailController,
                   hintText: 'Username',
                   obscureText: false,
                 ),
-          
+
                 const SizedBox(height: 10),
-          
-                // password textfield
+
+                // Password textfield
                 MyTextField(
                   controller: passwordController,
                   hintText: 'Password',
                   obscureText: true,
                 ),
-          
+
                 const SizedBox(height: 10),
-          
-                // forgot password?
+
+                // Forgot password?
+                // Ivin: Lmao I did not think about this
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 25.0),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
+                    children: const [
                       Text(
                         'Forgot Password?',
-                        style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 255, 255, 255)),
                       ),
                     ],
                   ),
                 ),
-          
+
                 const SizedBox(height: 25),
-          
-                // sign in button
+
+                // Sign in button
                 MyButton(
-                  onTap: signUserIn,
+                  onTap: signIn,
                 ),
-          
+
                 const SizedBox(height: 50),
-          
+
                 // or continue with
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 25.0),
@@ -111,11 +152,12 @@ class LoginPage extends StatelessWidget {
                           color: Colors.grey[400],
                         ),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 10.0),
                         child: Text(
                           'Or',
-                          style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 255, 255, 255)),
                         ),
                       ),
                       Expanded(
@@ -127,32 +169,33 @@ class LoginPage extends StatelessWidget {
                     ],
                   ),
                 ),
-          
+
                 const SizedBox(height: 50),
-          
+
                 // google  sign in buttons
                 // Row(
                 //   mainAxisAlignment: MainAxisAlignment.center,
                 //   children: const [
                 //     // google button
                 //     SquareTile(imagePath: 'impact-circle/lib/images/google.png'),
-          
+
                 //     SizedBox(width: 25),
                 //   ],
                 // ),
-          
+
                 const SizedBox(height: 50),
-          
-                // not a member? register now
+
+                // Not a member? Register now
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
+                  children: const [
                     Text(
                       'Not a member?',
-                      style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
+                      style:
+                          TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
                     ),
-                    const SizedBox(width: 4),
-                    const Text(
+                    SizedBox(width: 4),
+                    Text(
                       'Register now',
                       style: TextStyle(
                         color: Colors.blue,
