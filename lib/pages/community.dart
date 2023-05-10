41f2d95c275cae974935684d1ad15bab4bbcb4e5
akshaya <akshayakrishnan318@gmail.com>
2023-05-10 23:03:59 +0530

    changes made to requests.dart

--- lib/pages/community.dart
+++ lib/pages/community.dart
@@ -1,9 +1,15 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:impact_circle/pages/profilepage.dart';
-import 'package:impact_circle/pages/requests.dart'; // Import requests.dart
+import 'package:impact_circle/pages/requests.dart';
 
-import 'login.dart';
+
+class Community {
+  final String name;
+  final String description;
+
+  Community({required this.name, required this.description});
+}
 
 class MyCommunity extends StatefulWidget {
   const MyCommunity({Key? key}) : super(key: key);
@@ -13,6 +19,8 @@ class MyCommunity extends StatefulWidget {
 }
 
 class _MyCommunityState extends State<MyCommunity> {
+  List<Community> _communities = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,154 +28,87 @@ class _MyCommunityState extends State<MyCommunity> {
         backgroundColor: const Color.fromARGB(255, 219, 79, 24),
         actions: [
           IconButton(
-            onPressed: () {},
-            icon: const Icon(
-              Icons.search,
-            ),
-          )
+              onPressed: () => _dialogBuilder(context),
+              icon: const Icon(
+                Icons.account_circle,
+              ))
         ],
         elevation: 0,
         centerTitle: true,
         title: const Text(
           "Community",
           style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              fontSize: 27),
+              color: Colors.white, fontWeight: FontWeight.bold, fontSize: 27),
         ),
       ),
-      body: Align(
-        alignment: Alignment.topCenter,
-        child: Container(
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.3),
-                  blurRadius: 5,
-                  offset: const Offset(0, 3),
-                )
-              ]),
-          height: 200,
-          child: Card(
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20)),
-            margin: const EdgeInsets.all(10),
-            child: ListTile(
-              title: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Text(
-                  "Cleaning",
-                  style: const TextStyle(
-                      fontWeight: FontWeight.bold, fontSize: 30),
-                  textAlign: TextAlign.left,
-                ),
-              ),
-              subtitle: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Text(
-                  "Tap to see all the cleaning events happening",
-                  textAlign: TextAlign.left,
-                ),
-              ),
-              // Add a new button to see requests
-              trailing: ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => Requests(),
+      body: ListView.builder(
+        itemCount: _communities.length,
+        itemBuilder: (context, index) {
+          final community = _communities[index];
+          return Align(
+            alignment: Alignment.topCenter,
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.3),
+                      blurRadius: 5,
+                      offset: const Offset(0, 3),
+                    )
+                  ]),
+              height: 200,
+              child: Card(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+                margin: const EdgeInsets.all(10),
+                child: ListTile(
+                  title: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Text(
+                      community.name,
+                      style:
+                          const TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                      textAlign: TextAlign.left,
                     ),
-                  );
-                },
-                child: const Text("See Requests"),
+                  ),
+                  subtitle: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Text(
+                      community.description,
+                      textAlign: TextAlign.left,
+                    ),
+                  ),
+                  trailing: SizedBox(
+                    height: 30,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => MyRequests(),
+                          ),
+                        );
+                      },
+                      child: const Text("View Requests"),
+                    ),
+                  ),
+                ),
               ),
             ),
-          ),
-        ),
+          );
+        },
       ),
-      drawer: Drawer(
-        child: ListView(
-          padding: const EdgeInsets.symmetric(vertical: 50),
-          children: <Widget>[
-            Icon(
-              Icons.account_circle,
-              size: 150,
-              color: Colors.grey[700],
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            const Divider(
-              height: 2,
-            ),
-            ListTile(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const MyCommunity()),
-                );
-              },
-              selectedColor: const Color.fromARGB(255, 219, 79, 24),
-              selected: true,
-              contentPadding:
-                  const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-              leading: const Icon(Icons.group),
-              title: const Text(
-              "Community",
-              style: TextStyle(color: Colors.black),
-            ),
-          ),
-          ListTile(
-            onTap: () {
-              nextScreenReplace(
-                  context,
-                  ProfilePage(
-                    userName: "userName",
-                    email: "email",
-                  ));
-            },
-            contentPadding:
-                const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-            leading: const Icon(Icons.group),
-            title: const Text(
-              "Profile",
-              style: TextStyle(color: Colors.black),
-            ),
-          ),
-GestureDetector(
-  onTap: () async {
-    await FirebaseAuth.instance.signOut();
-    Navigator.pushAndRemoveUntil(
-      context,
-      MaterialPageRoute(builder: (context) => Login(onClickedSignUp: () {  },)),
-      (route) => false,
-    );
-  },
-  child: const ListTile(
-    contentPadding:
-         EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-    leading:  Icon(Icons.exit_to_app),
-    title:  Text(
-      "Logout",
-      style: TextStyle(color: Colors.black),
-    ),
-  ),
-)
-
-        ],
-      )),
-      
-
-      floatingActionButton: FloatingActionButton(
+  floatingActionButton: FloatingActionButton(
         onPressed: () {
           showDialog(
             context: context,
             builder: (context) {
-              return const AddGroupDialog();
+              return AddGroupDialog(onAddCommunity: (name, description) {
+                setState(() {
+                  _communities.add(Community(name: name, description: description));
+                });
+              });
             },
           );
         },
@@ -178,22 +119,30 @@ GestureDetector(
           color: Colors.white,
           size: 30,
         ),
-      ),
-    );
-  }
+      ));
     
   }
+}
 
-  groupList() {}
 
 class AddGroupDialog extends StatefulWidget {
-  const AddGroupDialog({Key? key}) : super(key: key);
+    final Function(String, String) onAddCommunity;
 
+  const AddGroupDialog({Key? key, required this.onAddCommunity}) : super(key: key);
+ 
   @override
   _AddGroupDialogState createState() => _AddGroupDialogState();
 }
 
 class _AddGroupDialogState extends State<AddGroupDialog> {
+    final _nameController = TextEditingController();
+  final _descriptionController = TextEditingController();
+   @override
+  void dispose() {
+    _nameController.dispose();
+    _descriptionController.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Dialog(
@@ -202,13 +151,11 @@ class _AddGroupDialogState extends State<AddGroupDialog> {
         child: Container(
           padding: const EdgeInsets.all(16),
           child: Column(
-      
             children: [
               const Center(
-                child:  Text(
+                child: Text(
                   "Add a New Community",
                   style: TextStyle(
-                      
                     fontWeight: FontWeight.bold,
                     fontSize: 20,
                   ),
@@ -216,12 +163,14 @@ class _AddGroupDialogState extends State<AddGroupDialog> {
               ),
               const SizedBox(height: 16),
               TextFormField(
+                controller: _nameController,
                 decoration: const InputDecoration(
                   labelText: "Community Name",
                 ),
               ),
               const SizedBox(height: 16),
               TextFormField(
+                controller: _descriptionController,
                 decoration: const InputDecoration(
                   labelText: "Community Description",
                 ),
@@ -229,13 +178,14 @@ class _AddGroupDialogState extends State<AddGroupDialog> {
               const SizedBox(height: 16),
               ElevatedButton(
                 onPressed: () {
+                  widget.onAddCommunity(_nameController.text, _descriptionController.text);
                   Navigator.pop(context);
                 },
                 style: ButtonStyle(
                   backgroundColor: MaterialStateProperty.all<Color>(
                       const Color.fromARGB(255, 219, 79, 24)),
                 ),
-                child: const Center(child: Text("Add Community")),
+                child: const Center(child: Text("Add Community",style: TextStyle(color: Colors.white),)),
               ),
             ],
           ),
@@ -249,3 +199,34 @@ void nextScreenReplace(context, page) {
   Navigator.pushReplacement(
       context, MaterialPageRoute(builder: (context) => page));
 }
+
+ Future<void> _dialogBuilder(BuildContext context) {
+  final user = FirebaseAuth.instance.currentUser!;
+  final email = user.email;
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('$email',),
+          content: const Text('A dialog is a type of modal window that\n'
+              'appears in front of app content to\n'
+              'provide critical information, or prompt\n'
+              'for a decision to be made.'),
+          actions: <Widget>[
+            Center(
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  textStyle: Theme.of(context).textTheme.labelLarge,
+                ),
+                child: const Text('Logout'),
+                onPressed: () 
+                  async {
+    await FirebaseAuth.instance.signOut();
+                },
+              ),
+            ),
+
+          ],
+        );
+      },
+    );} 
--- lib/pages/requests.dart
+++ lib/pages/requests.dart
@@ -1,143 +1,145 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:impact_circle/pages/community.dart';
 import 'package:impact_circle/pages/profilepage.dart';
 
-void main() => runApp(const Requests());
+class Requests {
+  final String name;
+  final String description;
 
-class Requests extends StatelessWidget {
-  const Requests({super.key});
-
-  static const String _title = 'name of community';
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        scaffoldBackgroundColor: Color(0xFFFFB8B8),
-      ),
-      title: _title,
-      home: Scaffold(
-        appBar: AppBar(
-            leading: IconButton(
-              icon: const Icon(Icons.arrow_back),
-              onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => MyCommunity(),
-                    ),
-                  );
-                },
-            ),
-            title: const Text(_title),
-            backgroundColor: Color(0xFFFFB8B8),
-            actions: <Widget>[
-    IconButton(
-      icon: const Icon(Icons.account_circle),
-      onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => ProfilePage(
-                    userName: "userName",
-                    email: "email",
-                  ),
-                    ),
-                  );
-                },
-    ),
-  ],
-            ),
-           
-        body:  MyStatelessWidget(),
-        floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showDialog(
-            context: context,
-            builder: (context) {
-              return const AddRequestDialog();
-            },
-          );
-        },
-        elevation: 0,
-        backgroundColor: Color(0xFFFFB8B8),
-        child: const Icon(
-          Icons.add,
-          color: Colors.white,
-          size: 30,
-        ),
-      ),
-      ),
-    );
-  }
+  Requests({required this.name, required this.description});
 }
 
-class MyStatelessWidget extends StatelessWidget {
-  
+class MyRequests extends StatefulWidget {
+  const MyRequests({Key? key}) : super(key: key);
 
-  final List<Map<String, String>> cardsData = [
-  {'title': 'Card 1', 'description': 'Description for card 1'},
-  {'title': 'Card 2', 'description': 'Description for card 2'},
-  {'title': 'Card 3', 'description': 'Description for card 3'},
-  {'title': 'Card 4', 'description': 'Description for card 4'},
-];
+  @override
+  State<MyRequests> createState() => _MyRequestsState();
+}
+
+class _MyRequestsState extends State<MyRequests> {
+  List<Requests> _requests = [];
+
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: cardsData.map((data) {
-            return Container(
-              width: double.infinity,
-              height: 200,
-              child: Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                elevation: 5,
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      
-                      Text(
-                        
-                        data['title'] ?? '',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 219, 79, 24),
+          actions: [
+            IconButton(
+                onPressed: () => _dialogBuilder(context),
+                icon: const Icon(
+                  Icons.account_circle,
+                ))
+          ],
+          elevation: 0,
+          centerTitle: true,
+          title: const Text(
+            "Community Name",
+            style: TextStyle(
+                color: Colors.white, fontWeight: FontWeight.bold, fontSize: 27),
+          ),
+        ),
+        body: Container(
+          color: Color.fromARGB(255, 233, 128, 86),
+          child: ListView.builder(
+            itemCount: _requests.length,
+            itemBuilder: (context, index) {
+              final community = _requests[index];
+              return Align(
+                alignment: Alignment.topCenter,
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                          color:
+                              Color.fromARGB(255, 255, 81, 0).withOpacity(0.3),
+                          blurRadius: 5,
+                          offset: const Offset(0, 3),
+                        )
+                      ]),
+                  height: 200,
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    margin: const EdgeInsets.all(10),
+                    child: ListTile(
+                      title: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Text(
+                          community.name,
+                          style: const TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 30),
+                          textAlign: TextAlign.left,
                         ),
                       ),
-                      SizedBox(height: 10),
-                      Text(
-                        data['description'] ?? '',
-                        style: TextStyle(
-                          fontSize: 14,
+                      subtitle: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Text(
+                          community.description,
+                          textAlign: TextAlign.left,
                         ),
                       ),
-                    ],
+                      trailing: SizedBox(
+                        height: 30,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            //volunteer for that request
+                          },
+                          child: const Text("Volunteer"),
+                          style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all<Color>(Color.fromARGB(255, 219, 79, 24)),
+                          ),
+                        ),
+                      ),
+                    ),
                   ),
                 ),
-              ),
-            );
-          }).toList(),
+              );
+            },
+          ),
         ),
-      ),
-    );
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (context) {
+                return AddRequestDialog(onAddRequests: (name, description) {
+                  setState(() {
+                    _requests
+                        .add(Requests(name: name, description: description));
+                  });
+                });
+              },
+            );
+          },
+          elevation: 0,
+          backgroundColor: const Color.fromARGB(255, 219, 79, 24),
+          child: const Icon(
+            Icons.add,
+            color: Colors.white,
+            size: 30,
+          ),
+        ));
   }
 }
 
 class AddRequestDialog extends StatefulWidget {
-  const AddRequestDialog({Key? key}) : super(key: key);
+  final Function(String, String) onAddRequests;
+
+  const AddRequestDialog({Key? key, required this.onAddRequests})
+      : super(key: key);
 
   @override
   _AddRequestDialogState createState() => _AddRequestDialogState();
 }
 
 class _AddRequestDialogState extends State<AddRequestDialog> {
+  final _nameController = TextEditingController();
+  final _descriptionController = TextEditingController();
+  final _volunteerController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Dialog(
@@ -146,13 +148,11 @@ class _AddRequestDialogState extends State<AddRequestDialog> {
         child: Container(
           padding: const EdgeInsets.all(16),
           child: Column(
-      
             children: [
               const Center(
-                child:  Text(
+                child: Text(
                   "Add a New Request",
                   style: TextStyle(
-                      
                     fontWeight: FontWeight.bold,
                     fontSize: 20,
                   ),
@@ -160,18 +160,21 @@ class _AddRequestDialogState extends State<AddRequestDialog> {
               ),
               const SizedBox(height: 16),
               TextFormField(
+                controller: _nameController,
                 decoration: const InputDecoration(
                   labelText: "Request Name",
                 ),
               ),
               const SizedBox(height: 16),
               TextFormField(
+                controller: _descriptionController,
                 decoration: const InputDecoration(
                   labelText: "Request Description",
                 ),
               ),
               const SizedBox(height: 16),
               TextFormField(
+                controller: _volunteerController,
                 decoration: const InputDecoration(
                   labelText: "No of volunteers required",
                 ),
@@ -179,6 +182,8 @@ class _AddRequestDialogState extends State<AddRequestDialog> {
               const SizedBox(height: 16),
               ElevatedButton(
                 onPressed: () {
+                  widget.onAddRequests(
+                      _nameController.text, _descriptionController.text);
                   Navigator.pop(context);
                 },
                 style: ButtonStyle(
@@ -195,5 +200,39 @@ class _AddRequestDialogState extends State<AddRequestDialog> {
   }
 }
 
+void nextScreenReplace(context, page) {
+  Navigator.pushReplacement(
+      context, MaterialPageRoute(builder: (context) => page));
+}
 
-
+Future<void> _dialogBuilder(BuildContext context) {
+  final user = FirebaseAuth.instance.currentUser!;
+  final email = user.email;
+  return showDialog<void>(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text(
+          '$email',
+        ),
+        content: const Text('A dialog is a type of modal window that\n'
+            'appears in front of app content to\n'
+            'provide critical information, or prompt\n'
+            'for a decision to be made.'),
+        actions: <Widget>[
+          Center(
+            child: TextButton(
+              style: TextButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              child: const Text('Logout'),
+              onPressed: () async {
+                await FirebaseAuth.instance.signOut();
+              },
+            ),
+          ),
+        ],
+      );
+    },
+  );
+}
