00e1ccf30452dc6337df51fef345eac8903ea100
LiquidFlare <ivinjoelabraham@gmail.com>
2023-05-11 08:53:17 +0530

    get username from backend

--- lib/pages/community_page.dart
+++ lib/pages/community_page.dart
@@ -2,7 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:impact_circle/pages/request.dart';
 
-
 class Community {
   final String name;
   final String description;
@@ -23,103 +22,105 @@ class _MyCommunityState extends State<MyCommunity> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 219, 79, 24),
-        actions: [
-          IconButton(
-              onPressed: () => _dialogBuilder(context),
-              icon: const Icon(
-                Icons.account_circle,
-              ))
-        ],
-        elevation: 0,
-        centerTitle: true,
-        title: const Text(
-          "Community",
-          style: TextStyle(
-              color: Colors.white, fontWeight: FontWeight.bold, fontSize: 27),
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 219, 79, 24),
+          // User profile
+          actions: [
+            IconButton(
+                onPressed: () => _dialogBuilder(context),
+                icon: const Icon(
+                  Icons.account_circle,
+                ))
+          ],
+
+          elevation: 0,
+          centerTitle: true,
+          title: const Text(
+            "Community",
+            style: TextStyle(
+                color: Colors.white, fontWeight: FontWeight.bold, fontSize: 27),
+          ),
         ),
-      ),
-      body: ListView.builder(
-        itemCount: _communities.length,
-        itemBuilder: (context, index) {
-          final community = _communities[index];
-          return Align(
-            alignment: Alignment.topCenter,
-            child: Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.3),
-                      blurRadius: 5,
-                      offset: const Offset(0, 3),
-                    )
-                  ]),
-              height: 250,
-              child: Card(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)),
-                margin: const EdgeInsets.all(10),
-                child: ListTile(
-                  title: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Text(
-                      community.name,
-                      style:
-                          const TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
-                      textAlign: TextAlign.left,
+        body: ListView.builder(
+          itemCount: _communities.length,
+          itemBuilder: (context, index) {
+            final community = _communities[index];
+            return Align(
+              alignment: Alignment.topCenter,
+              child: Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.3),
+                        blurRadius: 5,
+                        offset: const Offset(0, 3),
+                      )
+                    ]),
+                height: 200,
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  margin: const EdgeInsets.all(10),
+                  child: ListTile(
+                    title: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Text(
+                        community.name,
+                        style: const TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 30),
+                        textAlign: TextAlign.left,
+                      ),
                     ),
-                  ),
-                  subtitle: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Text(
-                      community.description,
-                      textAlign: TextAlign.left,
+                    subtitle: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Text(
+                        community.description,
+                        textAlign: TextAlign.left,
+                      ),
                     ),
-                  ),
-                  trailing: SizedBox(
-                    height: 30,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) =>  const MyRequests(),
-                          ),
-                        );
-                      },
-                      child: const Text("View Requests"),
+                    trailing: SizedBox(
+                      height: 30,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => Requests(),
+                            ),
+                          );
+                        },
+                        child: const Text("View Requests"),
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          );
-        },
-      ),
-  floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showDialog(
-            context: context,
-            builder: (context) {
-              return AddGroupDialog(onAddCommunity: (name, description) {
-                setState(() {
-                  _communities.add(Community(name: name, description: description));
-                });
-              });
-            },
-          );
-        },
-        elevation: 0,
-        backgroundColor: const Color.fromARGB(255, 219, 79, 24),
-        child: const Icon(
-          Icons.add,
-          color: Colors.white,
-          size: 30,
+            );
+          },
         ),
-      ));
-    
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (context) {
+                return AddGroupDialog(onAddCommunity: (name, description) {
+                  setState(() {
+                    _communities
+                        .add(Community(name: name, description: description));
+                  });
+                });
+              },
+            );
+          },
+          elevation: 0,
+          backgroundColor: const Color.fromARGB(255, 219, 79, 24),
+          child: const Icon(
+            Icons.add,
+            color: Colors.white,
+            size: 30,
+          ),
+        ));
   }
 }
 
@@ -134,9 +135,56 @@ class AddGroupDialog extends StatefulWidget {
 }
 
 class _AddGroupDialogState extends State<AddGroupDialog> {
-    final _nameController = TextEditingController();
-  final _descriptionController = TextEditingController();
-   @override
+  final database = FirebaseDatabase.instance.ref();
+
+  // Try to save to Realtime Database
+  void saveToDatabase(String nodeID) async {
+    try {
+      await database.child('/communities/$nodeID').set({
+        'name': commNameController.text,
+        'description': commDescController.text
+      });
+    } catch (e) {
+      print(e); //! Show to user and cancel save
+    }
+  }
+
+  // Performs three parts:
+  // Generating random ID for node, checking if ID is already used and finally
+  // Saving it to database.
+  void intiateSave() {
+    String nodeID = generateRandomString(8);
+    do {
+      nodeID = generateRandomString(8);
+    } while (checkIfNodeExists('/communities/$nodeID'));
+
+    saveToDatabase(nodeID);
+  }
+
+  // Generate random ID from characters from $chars
+  String generateRandomString(int len) {
+    var r = Random();
+    const chars = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz';
+    return List.generate(len, (index) => chars[r.nextInt(chars.length)]).join();
+  }
+
+  // Check if ID is already used
+  bool checkIfNodeExists(String node) {
+    bool nodeExists = false;
+
+    database.child('/communities/$node').onValue.listen((DatabaseEvent event) {
+      if (event.snapshot.value != null) {
+        nodeExists = true;
+      }
+    });
+
+    return nodeExists;
+  }
+
+  final commNameController = TextEditingController();
+  final commDescController = TextEditingController();
+
+  @override
   void dispose() {
     _nameController.dispose();
     _descriptionController.dispose();
@@ -199,7 +247,36 @@ void nextScreenReplace(context, page) {
       context, MaterialPageRoute(builder: (context) => page));
 }
 
- Future<void> _dialogBuilder(BuildContext context) {
+String? getUsernameFromString(String userDataString) {
+  RegExp regExp = RegExp(r'username:\s*(\S.*?)\s*(?=}})');
+  Match? match = regExp.firstMatch(userDataString);
+  if (match != null) {
+    var username = match.group(1);
+    return username;
+  } else {
+    print('No username found in string');
+    return null;
+  }
+}
+
+final databaseReference = FirebaseDatabase.instance.ref('users');
+
+void searchDatabase(String? valueToSearch) {
+  databaseReference
+      .orderByChild('email')
+      .equalTo(valueToSearch)
+      .onValue
+      .listen((event) {
+    if (event.snapshot.value != null) {
+      print('Found value in database!');
+      getUsernameFromString(event.snapshot.value.toString());
+    } else {
+      print('Value not found in database.');
+    }
+  });
+}
+
+Future<void> _dialogBuilder(BuildContext context) {
   final user = FirebaseAuth.instance.currentUser!;
   final email = user.email;
     return showDialog<void>(
@@ -241,14 +318,25 @@ void nextScreenReplace(context, page) {
           fontSize: 18.0,
           color: Colors.white,
         ),
-      ),
-      onPressed: () async {
-        await FirebaseAuth.instance.signOut();
-         Navigator.of(context).pop();
-      },
-    ),
-  ],
-);
-
-      },
-    );} 
+        content: const Text('A dialog is a type of modal window that\n'
+            'appears in front of app content to\n'
+            'provide critical information, or prompt\n'
+            'for a decision to be made.'),
+        actions: <Widget>[
+          Center(
+            child: TextButton(
+              style: TextButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              child: const Text('Logout'),
+              onPressed: () async {
+                Navigator.pop(context);
+                await FirebaseAuth.instance.signOut();
+              },
+            ),
+          ),
+        ],
+      );
+    },
+  );
+}
