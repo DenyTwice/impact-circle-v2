ce6bfdedffee147d79ea1ea9345311071bc283a9
govindkrishna03 <govindkrishna28@gmail.com>
2023-05-11 17:59:38 +0530

    fixed button names

--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:impact_circle/main.dart';
 import 'package:flutter/gestures.dart';
 import 'package:impact_circle/components/my_textfield.dart';
+
 import '../components/my_button.dart';
 
 class Register extends StatefulWidget {
@@ -112,7 +113,9 @@ Widget build(BuildContext context) {
                    const SizedBox(height: 25),
                   // Sign in button
                   MyButton(
+                    text: 'Register',
                     onTap: signUp,
+                    
                   ),
                 const SizedBox(height: 20),
                 RichText(
@@ -123,10 +126,10 @@ Widget build(BuildContext context) {
                       color: Colors.black,
                       fontSize: 16,
                     ),
-                    text: 'Already a User?  ',
+                    text: 'Already a User?',
                     children: [
                       TextSpan(
-                        text: 'Sign Up',
+                        text: 'Sign In',
                         recognizer: TapGestureRecognizer()
                           ..onTap = widget.onClickedSignUp,
                         style: TextStyle(
@@ -146,148 +149,4 @@ Widget build(BuildContext context) {
   );
 }
 
-
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: const Color.fromARGB(111, 19, 110, 170),
-//       body: SafeArea(
-//           child: Center(
-//         child: SingleChildScrollView(
-//           child: Column(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             children: [
-//               // Logo
-//               Stack(
-//                 alignment: Alignment.center,
-//                 children: const [
-//                   Icon(
-//                     Icons.circle_outlined,
-//                     size: 200,
-//                     color: Colors.white,
-//                   ),
-//                   Text(
-//                     'IMPACT',
-//                     style: TextStyle(
-//                       color: Colors.white,
-//                       fontWeight: FontWeight.bold,
-//                       fontSize: 26,
-//                     ),
-//                   ),
-//                 ],
-//               ),
-              
-//               const SizedBox(height: 20,),
-//               const Text(
-//                 'REGISTER',
-//                 style: TextStyle(
-//                     color: Color.fromARGB(255, 255, 255, 255),
-//                     fontSize: 32,
-//                     fontWeight: FontWeight.w900),
-//               ),
-//               // Username
-//               const SizedBox(height: 20),
-//               Padding(
-//                 padding: const EdgeInsets.symmetric(horizontal: 25.0),
-//                 child: TextField(
-//                     controller: usernameController,
-//                     obscureText: false,
-//                     textAlign: TextAlign.center,
-//                     decoration: InputDecoration(
-//                       enabledBorder: const OutlineInputBorder(
-//                         borderSide: BorderSide(color: Colors.white),
-//                       ),
-//                       hintText: 'Username',
-//                       fillColor: Colors.grey.shade200,
-//                       filled: true,
-//                       focusedBorder: OutlineInputBorder(
-//                         borderRadius: BorderRadius.circular(50.0),
-//                         borderSide: const BorderSide(color: Colors.white),
-//                       ),
-//                     )),
-//               ),
-//               // Email
-//               const SizedBox(height: 20),
-//               Padding(
-//                 padding: const EdgeInsets.symmetric(horizontal: 25.0),
-//                 child: TextField(
-//                     controller: emailController,
-//                     textAlign: TextAlign.center,
-//                     obscureText: false,
-//                     decoration: InputDecoration(
-//                       enabledBorder: const OutlineInputBorder(
-//                         borderSide: BorderSide(color: Colors.white),
-//                       ),
-//                       hintText: 'Email',
-//                       fillColor: Colors.grey.shade200,
-//                       filled: true,
-//                       focusedBorder: const OutlineInputBorder(
-//                         borderSide: BorderSide(color: Colors.white),
-//                       ),
-//                     )),
-//               ),
-//               // Password
-//               const SizedBox(height: 25),
-//               Padding(
-//                 padding: const EdgeInsets.symmetric(horizontal: 25.0),
-//                 child: TextField(
-//                     controller: passwordController,
-//                     textAlign: TextAlign.center,
-//                     obscureText: true,
-//                     decoration: InputDecoration(
-//                       enabledBorder: const OutlineInputBorder(
-//                         borderSide: BorderSide(color: Colors.white),
-//                       ),
-//                       hintText: 'Password',
-//                       fillColor: Colors.grey.shade200,
-//                       filled: true,
-//                       focusedBorder: const OutlineInputBorder(
-//                         borderSide: BorderSide(color: Colors.white),
-//                       ),
-//                     )),
-//               ),
-              // const SizedBox(height: 25),
-              // SizedBox(
-              //   height: 60,
-              //   width: 300,
-              //   child: ElevatedButton(
-              //     onPressed: signUp,
-              //     style: ButtonStyle(
-              //       shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-              //         RoundedRectangleBorder(
-              //           borderRadius: BorderRadius.circular(30.0),
-              //         ),
-              //       ),
-              //       backgroundColor:
-              //           MaterialStateProperty.all<Color>(Colors.grey.shade200),
-              //     ),
-              //     child: const Text(
-              //       'Sign Up',
-              //       style: TextStyle(color: Colors.black),
-              //     ),
-              //   ),
-              // ),
-              // const SizedBox(height: 25),
-              // RichText(
-              //     text: TextSpan(
-              //         recognizer: TapGestureRecognizer(),
-              //         style: const TextStyle(color: Colors.white, fontSize: 20),
-              //         text: 'Already a User?  ',
-              //         children: [
-              //       TextSpan(
-              //           text: 'Login',
-              //           recognizer: TapGestureRecognizer()
-              //             ..onTap = widget.onClickedSignUp,
-              //           style: TextStyle(
-              //               decoration: TextDecoration.underline,
-              //               color: Theme.of(context).colorScheme.secondary))
-              //     ]))
-//             ],
-//           ),
-//         ),
-//       )),
-//     );
-//   }
-// }
 }
